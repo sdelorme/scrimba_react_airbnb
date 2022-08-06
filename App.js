1bb5b71faad60804091eb3c2001d420bee1cc957
Seth Delorme <delorme.s.m@gmail.com>
2022-08-06 14:28:31 -0400

    add navar with logo and styling

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Navbar from "./Components/Navbar"
+
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <h1>App component</h1>
+        </div>
+    )
+}
