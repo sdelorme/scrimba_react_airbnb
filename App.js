5cac746c3363d309f9c87882fc53f4f071f888a2
Seth Delorme <delorme.s.m@gmail.com>
2022-08-09 12:09:50 -0400

    updated airbnb project progress

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import React from "react"
-import Navbar from "./Components/Navbar"
-
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
 
 export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })        
+    
+            // <Hero />
     return (
         <div>
             <Navbar />
-            <h1>App component</h1>
+            <section className="cards-list">
+                {cards}
+            </section>
         </div>
     )
 }
